8e1c4a1baa266f6cf0087f3206e40701a3f20703
AdishRao <Adish.rao98@gmail.com>
2022-01-17 19:05:37 -0500

    End of Tools tutorial

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    React.createElement(Pet, { name: "Luxa", animal: "Dog", breed: "Husky" }),
+    React.createElement(Pet, {
+      name: "Luxa",
+      animal: "Dog",
+      breed: "Husky",
+    }),
     React.createElement(Pet, {
       name: "Charlie",
       animal: "Dog",
